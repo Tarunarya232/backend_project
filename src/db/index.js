305d71541311bd7f2308efef0146012033fc4e83
Tarun <108832747+Tarunarya232@users.noreply.github.com>
2025-06-01 20:17:15 +0530

    Create index.js

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constant.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MongoDB Connected !!! Database Host: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log('MongoDB Connection Error: ', error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
