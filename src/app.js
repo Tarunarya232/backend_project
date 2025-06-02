b57d151e24c79a0b9e0bea1fe2483314249bb202
Tarun <108832747+Tarunarya232@users.noreply.github.com>
2025-06-02 13:57:12 +0530

    Update app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:'16kb'}));
+app.use(express.urlencoded({extended:true, limit: '16kb' }));
+app.use(express.static());
+app.use(cookieParser());
+
+export {app};
