ae0511bbc72a9e39e931c7f80dc2dffa8fe6b7e1
Tarun <108832747+Tarunarya232@users.noreply.github.com>
2025-06-04 18:11:44 +0530

    Create cloudinary.js

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,39 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath){ //If local file path doesn't exist
+            return null;  
+        } else{ //If file path did exists
+            // Upload an image
+            const uploadResult = await cloudinary.uploader
+            .upload(localFilePath, {
+                    resource_type: "auto",
+                }
+            )
+            .catch((error) => {
+                console.log(error);
+            });
+        
+            console.log("file is uploaded on cloudinary", uploadResult.url);
+            return uploadResult;
+        }
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+    
+export {uploadOnCloudinary};
