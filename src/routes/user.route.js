4c90b40411641a08262a5c735cfb9e19138b6c39
Tarun <108832747+Tarunarya232@users.noreply.github.com>
2025-06-10 10:57:57 +0530

    Update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,7 +5,7 @@ import {upload} from "../middlewares/multer.middleware.js";
 const userRouter = Router();
 
 userRouter.route("/register").post(
-    upload.fields(
+    upload.fields([
         {
             name:"avatar",
             maxCount:1
@@ -14,7 +14,7 @@ userRouter.route("/register").post(
             name: "coverImage",
             maxCount:1
         }
-    ),
+    ]),
     registerUser);
 
 export default userRouter;
