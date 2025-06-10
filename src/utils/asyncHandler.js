b7343391bdcff4bd99f2469dabc670cbe4b4851c
Tarun <108832747+Tarunarya232@users.noreply.github.com>
2025-06-10 09:41:49 +0530

    Update asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,15 +1,14 @@
-
 const asyncHandler = (func) => {
-    async (req, res, next) =>{
-      try {
-        await func(req, res, next);
-      } catch (error) {
-        res.status(error.code || 500).json({
-            success:false,
-            message:error.message
-        })
-      } 
+  return async (req, res, next) => {
+    try {
+      await func(req, res, next);
+    } catch (error) {
+      res.status(error.code || 500).json({
+        success: false,
+        message: error.message,
+      });
     }
-}
+  };
+};
 
-export {asyncHandler};
+export { asyncHandler };
