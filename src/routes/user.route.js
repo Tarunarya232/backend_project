e18d83d85e2f75b683ce0a0bbd55b2008ceb40b6
Tarun <108832747+Tarunarya232@users.noreply.github.com>
2025-06-11 19:06:42 +0530

    Update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
@@ -16,5 +17,12 @@ userRouter.route("/register").post(
         }
     ]),
     registerUser);
+
+userRouter.route("/login").post(
+    loginUser
+)
+
+//Secured Routes -- Routes which have acccess only after we login
+userRouter.route("/logout").post(verifyJWT, logoutUser);//Now, we will have access to user object as we have added it to our request object
 
 export default userRouter;
